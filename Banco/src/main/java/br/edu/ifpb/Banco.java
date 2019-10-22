ff8837dfc4da47551a3997538ef890e136b6e728
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2019-10-22 15:28:00 -0300

    Criando arquivo de testes e adicionando sobrecarga da função remover_Conta

--- Banco/src/main/java/br/edu/ifpb/Banco.java
+++ Banco/src/main/java/br/edu/ifpb/Banco.java
@@ -9,6 +9,7 @@ public class Banco implements Iterable<Conta>, AutoCloseable {
 
     public Banco() { this("--sem nome--", Comparator.naturalOrder()); }
     public Banco(String nome) { this(nome, Comparator.naturalOrder()); }
+    public Banco(Comparator<Conta> comparator) { this("--sem nome--", comparator); }
     public Banco(String nome, Comparator<Conta> comparator) {
         this.nome = Objects.requireNonNullElse(nome, "--sem nome--");
         Comparator<Conta> comp = Objects.requireNonNullElse(comparator, Comparator.naturalOrder());
@@ -24,8 +25,8 @@ public class Banco implements Iterable<Conta>, AutoCloseable {
     public boolean adicionar_Conta(Conta conta) {
         return contas.add(conta);
     }
-    public boolean remover_Conta(int numero) throws ContaException { return contas.remove(buscar_Conta(numero));
-    }
+    public boolean remover_Conta(int numero) throws ContaException { return contas.remove(buscar_Conta(numero)); }
+    public boolean remover_Conta(Conta c) { return contas.remove(c); }
     public Conta buscar_Conta(int numero) throws ContaException {
         return contas.stream()
                 .filter(conta -> conta.getNumero() == numero)
@@ -41,6 +42,11 @@ public class Banco implements Iterable<Conta>, AutoCloseable {
         return string.toString();
     }
     public String exibir_Conta(int numero) throws ContaException { return buscar_Conta(numero).toString(); }
+    public String exibir_Contas() {
+        StringBuilder string = new StringBuilder();
+        for (Conta c : contas) { string.append(c); }
+        return string.toString();
+    }
 
     @Override
     public Iterator<Conta> iterator() { return contas.iterator(); }
--- Banco/src/test/java/br/edu/ifpb/AppTest.java
+++ Banco/src/test/java/br/edu/ifpb/AppTest.java
@@ -1,20 +1,39 @@
 package br.edu.ifpb;
 
 import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    Banco banco = new Banco();
+    Conta c1 = new Conta(0, "José", 3456.90);
+    Conta c2 = new Conta(1, "v5u6vu7j", 3456.90);
+    Conta c3 = new Conta(2, "tfx rt34", 3456.90);
+    Conta c4 = new Conta(3, "Arthur", 3456.90);
+    Conta c4Clone = new Conta(3, "Arthur", 3456.90);
+
+    @Before
+    public void adiciona_Contas() {
+        banco.adicionar_Conta(c1);
+        banco.adicionar_Conta(c2);
+        banco.adicionar_Conta(c3);
+        banco.adicionar_Conta(c4);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void adicionou_Conta() {
+        banco.adicionar_Conta(c4Clone);
+
+        assertThat(banco, contains(c1, c2, c3, c4));
+    }
+
+    @Test
+    public void removeu_Conta() {
+        banco.remover_Conta(c2);
+
+        assertThat(banco, not(contains(c2)));
     }
 }
