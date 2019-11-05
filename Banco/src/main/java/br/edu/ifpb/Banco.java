f451f7fa98271b8e37bf8ff465753904d71aba73
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2019-11-05 09:59:32 -0300

    Mudanças em Banco: 	1 - Função para filtrar contas agora filtra por número e saldo 	2 - Função para converter contas de um TXT em um HashSet agora faz um replace na vírgula Novos: 	1 - Acionado TXT com contas

--- Banco/src/main/java/br/edu/ifpb/Banco.java
+++ Banco/src/main/java/br/edu/ifpb/Banco.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Banco implements Iterable<Conta>, AutoCloseable {
     public static final String CONTAS_TXT = "contas.txt";
@@ -23,6 +23,9 @@ public class Banco implements Iterable<Conta>, AutoCloseable {
 
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = Objects.requireNonNullElse(nome, "--sem nome--"); }
+
+    public Set<Conta> getContas() { return Collections.unmodifiableSet(contas); }
+
     public void start_Contas() throws IOException, DadosException {
         if (contas.isEmpty()) {
             File file = new File(CONTAS_TXT);
@@ -33,7 +36,8 @@ public class Banco implements Iterable<Conta>, AutoCloseable {
                 Set<String> AUX = new HashSet<>(Files.readAllLines(Path.of(CONTAS_TXT)));
                 for (String c : AUX) {
                     String[] AUX1 = c.split(" ");
-                    contas.add(new Conta(Integer.parseInt(AUX1[0]), AUX1[1], Double.parseDouble(AUX1[2])));
+                    String AUX2 = AUX1[2].replace(',', '.');
+                    contas.add(new Conta(Integer.parseInt(AUX1[0]), AUX1[1], Double.parseDouble(AUX2)));
                 }
             }
             else throw new DadosException("O banco de dados não existe!");
@@ -51,15 +55,13 @@ public class Banco implements Iterable<Conta>, AutoCloseable {
         return contas.stream()
                 .filter(conta -> conta.getNumero() == numero).findFirst().orElseThrow(() -> new ContaException("A conta não existe"));
     }
-    public String filtrar_Conta(int numero, int numero1) throws ContaException {
-        TreeSet<Conta> contas_FIL = new TreeSet<>(Comparator.naturalOrder());
-        contas_FIL.addAll(contas.stream()
-                .filter(Conta -> Conta.getNumero() >= numero && Conta.getNumero() <= numero1).collect(Collectors.toSet()));
-        StringBuilder string = new StringBuilder(" ");
-        for (Conta c : contas) {
-            string.append(c);
-        }
-        return string.toString();
+    public Set<Conta> filtrar_Conta(int numero, int numero1, String mod) throws ContaException {
+        Set<Conta> AUX = null;
+        if (mod.equalsIgnoreCase("Saldo")) AUX = contas.stream()
+                .filter(Conta -> Conta.getSaldo() >= numero && Conta.getSaldo() <= numero1).collect(Collectors.toSet());
+        if(mod.equalsIgnoreCase("Número")) AUX = contas.stream()
+                .filter(Conta -> Conta.getNumero() >= numero && Conta.getNumero() <= numero1).collect(Collectors.toSet());
+        return AUX;
     }
     public String exibir_Conta(int numero) throws ContaException { return buscar_Conta(numero).toString(); }
     public String exibir_Conta() {
