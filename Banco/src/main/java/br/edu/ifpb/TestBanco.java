e75417f5ec30b1f399e60b771afbe654a4d14132
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2019-11-05 12:04:06 -0300

    Concluindo TestBanco

--- Banco/src/main/java/br/edu/ifpb/TestBanco.java
+++ Banco/src/main/java/br/edu/ifpb/TestBanco.java
@@ -1,12 +1,13 @@
 package br.edu.ifpb;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class TestBanco {
     public static void main(String[] args) {
-
+        start_Test_Banco();
     }
 
     public static void show_Gui(String name) {
@@ -14,20 +15,22 @@ public class TestBanco {
         System.out.println("1 - Adcionar conta\n" +
                         "2 - Remover conta\n" +
                         "3 - Filtrar contas\n" +
-                        "4 - Buscar conta\n" +
-                        "5 - Exibir dados da conta\n" +
-                        "6 - Exibir todas as contas\n" +
-                        "7 - Total de contas\n" +
-                        "8 - Gravar contas\n" +
+                        "4 - Exibir dados da conta\n" +
+                        "5 - Exibir todas as contas\n" +
+                        "6 - Total de contas\n" +
+                        "7 - Gravar contas\n" +
                         "8 - Sair\n" +
                         "  Digite uma opção: ");
     }
 
     public static void start_Test_Banco() {
-        try (Banco b = new Banco("Banco do Brasil - BB");) {
-            int cond = 0;
-            Scanner input = new Scanner(System.in);
-            while (cond != 8) {
+        int cond = 0;
+        boolean cond1 = true;
+        Banco b = new Banco("Banco do Brasil - BB");
+        while (cond1) {
+            try (b) {
+                b.start_Contas();
+                Scanner input = new Scanner(System.in);
                 show_Gui("Banco do Brasil - BB");
                 cond = input.nextInt();
                 switch (cond) {
@@ -39,9 +42,9 @@ public class TestBanco {
                         System.out.print("Saldo: ");
                         double saldo = input.nextDouble();
                         if (b.adicionar_Conta(numero, nome, saldo)) {
-                            System.out.println("Conta adicionada!");
+                            System.out.println("Conta adicionada!\n");
                         }
-                        else throw new ContaException(String.format("Já existe conta com o número %s", numero));
+                        else throw new ContaException(String.format("Já existe conta com o número %s\n", numero));
                         break;
                     case 2 :
                         System.out.print("Número : ");
@@ -54,18 +57,39 @@ public class TestBanco {
                     case 3 :
                         System.out.print("Filtral por número ou saldo? ");
                         String mod = input.next();
-                        System.out.print("Primeiro número : ");
+                        System.out.print("Primeiro número: ");
                         numero = input.nextInt();
-                        System.out.print("Segundo número : ");
+                        System.out.print("Segundo número: ");
                         int numero1 = input.nextInt();
                         for (Conta c : b.filtrar_Conta(numero, numero1, mod)) {
                             System.out.println(c);
                         }
+                        break;
+                    case 4 :
+                        System.out.print("Número: ");
+                        numero = input.nextInt();
+                        System.out.printf("Conta: %s\n\n", b.exibir_Conta(numero));
+                        break;
+                    case 5 :
+                        System.out.printf("Contas:\n%s\n\n", b.exibir_Conta());
+                        break;
+                    case 6 :
+                        System.out.printf("Total de contas: %d\n\n", b.qtde_Contas());
+                        break;
+                    case 7 :
+                        throw new BancoException();
+                    case 8 :
+                        b.write_Contas();
+                        cond1 = false;
+                        break;
                 }
             }
-        }
-        catch (BancoException | ContaException | DadosException e) {
-            System.out.println(e);
+            catch (BancoException | ContaException | DadosException | IOException e) {
+                System.out.println(e.getMessage());
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Entrada inválida!\n");
+            }
         }
     }
 }
